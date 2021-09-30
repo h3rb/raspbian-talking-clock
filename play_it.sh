dede34867a785333a5bf17eddda9dd3ed04da00f
h3rb <hgilliland@gmail.com>
2021-09-30 12:08:27 -0400

    Update play_it.sh

--- play_it.sh
+++ play_it.sh
@@ -4,18 +4,20 @@ FILE=/var/www/html/messaging/now.txt
 
 i=0
 
-while [ $i -ne 10 ]
+while [ $i -ne 25 ]
 do
 
 if test -f "$FILE"; then
+   value=$(</var/www/html/messaging/now.txt)
+value=${value//$'\n'/} # Remove all newlines.
+value=${value%$'\n'}   # Remove a trailing newline.
    rm $FILE
-   sudo php -f /home/pi/run.php
-   # Sudo is required to read fortunes files.
+   sudo php -f /home/pi/run.php $value
 fi
 
-sleep 4
+sleep 2
 i=$[$i+1]
 
 done
 
-sudo pkill -9 -f play_it.sh
+#sudo pkill -9 -f play_it.sh
