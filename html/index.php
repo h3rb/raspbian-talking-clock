25379505011a882cf12117eba383c8b5552006f1
h3rb <hgilliland@gmail.com>
2021-09-30 12:08:03 -0400

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,3 +1,4 @@
+<html>
 <head>
 <title>Talking clock</title>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
@@ -7,11 +8,13 @@ crossorigin="anonymous"></script>
 <body>
  <button id="hush">Hush</button>
  <button id="play">Play</button>
+ <button id="brief">Brief</button>
 <script type="text/javascript">
  function Get(id) { return document.getElementById(id); }
  $(document).ready(function(e){
   $(Get("hush")).click( function(e){ window.location="/?hush=1"; });
   $(Get("play")).click( function(e){ window.location="/?play=1"; });
+  $(Get("brief")).click( function(e){ window.location="/?brief=1"; });
  });
 </script>
  <div id="state">
@@ -41,6 +44,12 @@ crossorigin="anonymous"></script>
  if ( isset($d['play']) && intval($d['play'])==1 ) {
   echo 'Playing...';
   file_put_contents('/var/www/html/messaging/now.txt','play');
+  die;
+ }
+
+ if ( isset($d['brief']) && intval($d['brief'])==1 ) {
+  echo 'Playing brief...';
+  file_put_contents('/var/www/html/messaging/now.txt','brief');
   die;
  }
 ?>
