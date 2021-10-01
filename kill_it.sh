fa1b702bb09cd8ed855f0d7bf7eefa49507e0436
h3rb <hgilliland@gmail.com>
2021-10-01 10:33:09 -0400

    Update kill_it.sh

--- kill_it.sh
+++ kill_it.sh
@@ -4,18 +4,18 @@ FILE=/var/www/html/messaging/kill.txt
 
 i=0
 
-while [ $i -ne 10 ]
+while [ $i -ne 25 ]
 do
 
 if test -f "$FILE"; then
    rm $FILE
-   sudo pkill -9 -f run.php
+   sudo pkill -9 -f "php -f run.php"
    sudo pkill -9 -f espeak
 fi
 
-sleep 4
+sleep 2
 i=$[$i+1]
 
 done
 
-sudo pkill -9 -f kill_it.sh
+#sudo pkill -9 -f kill_it.sh
